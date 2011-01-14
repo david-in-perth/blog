56440cc86792fcbbfcedbe4f792feb13cfacd7a8
David Prince <david.prince@agworld.com.au>
2011-01-14 15:55:12 +0800

    Second try at initial commit of 'blog' rails tutorial project, following the tutorial video at http://media.rubyonrails.org/video/rails_blog_2.mov.

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,15 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying cookie session data integrity.
+# If you change this key, all old sessions will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+ActionController::Base.session = {
+  :key         => '_blog_session',
+  :secret      => '2259bd48b102bd8ebbe0c11f0610d351d458ff41f10258f45eb01ec4565dc2a746694716009961f2c5964a34a20d2ca4454c02f40e52de48237a20367246c48d'
+}
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# ActionController::Base.session_store = :active_record_store
