8dabb83f6ae942bfff18ff4339d804afbee27309
David Prince <david.prince@agworld.com.au>
2011-01-17 12:23:12 +0800

    Implemented comments.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110114070149) do
+ActiveRecord::Schema.define(:version => 20110117032038) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "post_id"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
